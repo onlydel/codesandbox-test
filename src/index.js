c418b72cdc1b374dc2a8004f9033915c000393de
Won Hong, Cho <onlydel@realgrid.com>
2020-01-20 14:08:53 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>팀 샌드박스 테스트 입니다.</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
